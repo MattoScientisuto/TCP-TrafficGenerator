c49b3d020f53fe3371dabff62d38feee926f1678
MattoScientisuto <gmstegnokilla@gmail.com>
2024-09-28 17:22:44 -0600

    Now properly exits after process completes

--- TrafficGenerator.py
+++ TrafficGenerator.py
@@ -3,48 +3,61 @@
 # Random PCAP Traffic Generator
 
 # Created: September 25th, 2024
-# Last Updated: September 26th, 2024
+# Last Updated: September 28th, 2024
 # ============================================ #
 
 from DateTimeFetching import *
 
 import subprocess
+import sys
 import os
 import random
 import time
 
-confirmation_file = os.path.expanduser("~/confirm.txt")
 
 # Choose a random .pcap file and return as a string
 def choose_randpcap():
-    curr_pcap = random.choice(os.listdir(f"/home/{os.getlogin()}/pcaps"))
+    curr_pcap = random.choice(os.listdir(f"/home/cyber/pcaps"))
     return curr_pcap
 
 # Run the selected .pcap through tcpreplay
 def start_replay():
     
     replay_proc = subprocess.Popen(
-        ["sudo", "-S", "tcpreplay", "-i", "enp3s0", f"{choose_randpcap()}"],
-        # f'echo "Cron job executed at {get_timestamp()}" >> {confirmation_file}',
-        shell=True,
+        ["sudo", "-S", "tcpreplay", "-i", "enp3s0", "--topspeed", f"/home/cyber/pcaps/{choose_randpcap()}"],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        stderr=subprocess.PIPE,
+        text=True
     )
     
-    # Periodic check every 5 seconds to see if the .pcap replay is finished
+    # Periodic check every 10 seconds to see if the .pcap replay is finished
     while True:
-        
+
         status = replay_proc.poll()
-        
+
         if status is not None:
-            print(f"[{get_timestamp()}] tcp replay complete!")
+
+            print(f"[ {get_datestamp()} ][ {get_timestamp()} ] tcp replay complete!")
+
+            stdout_data, stderr_data = replay_proc.communicate()
+
+            if stdout_data:
+                print("=== tcpreplay output ===")
+                print(stdout_data)
+            if stderr_data:
+                print("=== tcpreplay errors ===")
+                print(stderr_data)
+
             return status
+
         else:
-            print(f"[{get_timestamp()}] not done yet, wait 5 seconds...")
-        time.sleep(5)
+            print(f"[ {get_datestamp()}][{get_timestamp()} ] not done yet, wait 10 seconds...")
+        time.sleep(10)
 
 
 if __name__ == "__main__":
     
-    start_replay()
+    status = start_replay()
+    sys.exit(status)
+
